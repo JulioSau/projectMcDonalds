232de7240d90ff625f9565ae0c9a3d492d1d27e3
JulioSau <tura_96@hotmail.com>
2020-08-25 14:47:55 +0000

    add footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
+import "../../styles/footer.css";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	<div className="footer mt-auto d-flex align-items-center">
+		<span className="offset-10 text-white">© D-Kitchen</span>
+	</div>
 );
