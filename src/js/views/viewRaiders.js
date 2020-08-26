8f3e7040288e7f96837a6adf49d401f6eb0e1bcb
riduarte <riduarte88@gmail.com>
2020-08-26 09:28:08 +0000

    Vista movil Raiders, solo planteado

--- src/js/views/viewRaiders.js
+++ src/js/views/viewRaiders.js
@@ -1,17 +1,20 @@
 import React from "react";
-import "../../styles/home.scss";
+import "../../styles/raiders.scss";
 import { TableInfo } from "../component/tableInfo";
 import { Link } from "react-router-dom";
 
-export const Raiders = () => (
+export const ViewRaiders = () => (
 	<div className="container">
 		<Link to="/" />
 		<Link to="/login">
-			<div>LOGUIN</div>
+			<div>LOGIN</div>
 		</Link>
 		<div className="row p-2 px-4 d-flex justify-content-between align-items-end">
-			{" "}
-			<h1>D-Kitchen</h1> <img src="https://dkitchenincubator.com/wp-content/uploads/2020/01/cocinero-1.png" />
+			<h1>D-Kitchen</h1>{" "}
+			<img
+				className="logo-raiders"
+				src="https://dkitchenincubator.com/wp-content/uploads/2020/01/cocinero-1.png"
+			/>
 		</div>
 		<TableInfo />
 	</div>
