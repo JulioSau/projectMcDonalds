3950cc526308841ddbc9a4c10e69aee05434b496
JulioSau <tura_96@hotmail.com>
2020-08-28 10:39:50 +0000

    algunas modificaciones simples

--- src/js/views/newcooker.js
+++ src/js/views/newcooker.js
@@ -5,27 +5,29 @@ export const NewCooker = () => {
 	return (
 		<div className="container-fluid d-flex justify-content-center">
 			<div className="col-5">
-				<h3 className="pb-3 pt-5">Crear nuevo usuario</h3>
+				<h3 className="pb-3 pt-5 text-center">Crear nuevo usuario</h3>
 				<form>
 					<div className="form-group">
 						<label>Nombre de usuario:</label>
-						<input type="user" className="form-control" id="user" />
+						<input type="user" className="input form-control rounded-pill" id="user" />
 					</div>
 					<div className="form-group">
 						<label>Nombre y apellidos:</label>
-						<input type="name" className="form-control" id="name" />
+						<input type="name" className="input form-control rounded-pill" id="name" />
 					</div>
 					<div className="form-group">
 						<label>Email address:</label>
-						<input type="email" className="form-control" id="email" />
+						<input type="email" className=" input form-control rounded-pill" id="email" />
 					</div>
 					<div className="form-group">
 						<label>Password:</label>
-						<input type="password" className="form-control" id="pwd" />
+						<input type="password" className="input form-control rounded-pill" id="pwd" />
 					</div>
-					<button type="submit" className="btn btn-default bg-success text-white ">
-						Crear nuevo usuario
-					</button>
+					<div className="text-center pt-5">
+						<button type="submit" className="submit btn btn-default bg-success text-white ">
+							Crear nuevo usuario
+						</button>
+					</div>
 				</form>
 			</div>
 		</div>
