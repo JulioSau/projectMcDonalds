f731e27ac3b860e7a7a67c54d497b6c2fbcfb41c
JulioSau <tura_96@hotmail.com>
2020-08-28 13:57:03 +0000

    modificaciones en el formulario de creacion de nuevo usuario por parte del admin

--- src/js/views/newcooker.js
+++ src/js/views/newcooker.js
@@ -3,28 +3,48 @@ import "../../styles/newcooker.scss";
 
 export const NewCooker = () => {
 	return (
-		<div className="container-fluid d-flex justify-content-center">
-			<div className="col-5">
-				<h3 className="pb-3 pt-5 text-center">Crear nuevo usuario</h3>
+		<div className="principalbox container rounded d-flex justify-content-center mt-5">
+			<div className="col-md-6">
+				<h2 className="pb-3 pt-5 text-center">Nuevo usuario</h2>
 				<form>
 					<div className="form-group">
 						<label>Nombre de usuario:</label>
-						<input type="user" className="input form-control rounded-pill" id="user" />
+						<input
+							type="user"
+							className="input form-control rounded-pill"
+							id="user"
+							placeholder="Escribe tu usuario"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Nombre y apellidos:</label>
-						<input type="name" className="input form-control rounded-pill" id="name" />
+						<input
+							type="name"
+							className="input form-control rounded-pill"
+							id="name"
+							placeholder="Escribe tu nombre y apellidos"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Email address:</label>
-						<input type="email" className=" input form-control rounded-pill" id="email" />
+						<input
+							type="email"
+							className=" input form-control rounded-pill"
+							id="email"
+							placeholder="Escribe tu email"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Password:</label>
-						<input type="password" className="input form-control rounded-pill" id="pwd" />
+						<input
+							type="password"
+							className="input form-control rounded-pill"
+							id="pwd"
+							placeholder="Escribe tu contraseña"
+						/>
 					</div>
-					<div className="text-center pt-5">
-						<button type="submit" className="submit btn btn-default bg-success text-white ">
+					<div className="text-center pt-4 pb-4">
+						<button type="submit" className="submit btn btn-default bg-success rounded-pill text-white ">
 							Crear nuevo usuario
 						</button>
 					</div>
